29af3d8afacdf3410a497ee7e94d2c635089cd6c
Ales Rechtorik <foxxcz@gmail.com>
2024-10-07 18:21:18 +0200

    wip

--- browser-esm-webpack/index.js
+++ browser-esm-webpack/index.js
@@ -1,26 +1,13 @@
 import { configureMonacoYaml } from 'monaco-yaml';
 
-self.MonacoEnvironment = {
+window.MonacoEnvironment = {
   getWorkerUrl: function (_, label) {
-    if (label === 'json') {
-      return './json.worker.bundle.js';
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return './css.worker.bundle.js';
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return './html.worker.bundle.js';
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return './ts.worker.bundle.js';
-    }
     if (label === 'yaml') {
-      return './yaml.worker.bundle.js';
+      return './yaml.worker.js';
     }
-    return './editor.worker.bundle.js';
+    return './editor.worker.js';
   }
 };
-
 
 export class MonacoService {
   constructor() {
--- browser-esm-webpack/webpack.config.js
+++ browser-esm-webpack/webpack.config.js
@@ -5,15 +5,10 @@ module.exports = {
   entry: {
     app: './index.js',
     'editor.worker': 'monaco-editor/esm/vs/editor/editor.worker.js',
-    'json.worker': 'monaco-editor/esm/vs/language/json/json.worker',
-    'css.worker': 'monaco-editor/esm/vs/language/css/css.worker',
-    'html.worker': 'monaco-editor/esm/vs/language/html/html.worker',
-    'ts.worker': 'monaco-editor/esm/vs/language/typescript/ts.worker',
     'yaml.worker': 'monaco-yaml/yaml.worker'
   },
   output: {
-    globalObject: 'self',
-    filename: '[name].bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
